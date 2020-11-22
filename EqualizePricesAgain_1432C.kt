836c421eb2a35a15d70b89cba03786aaa1c0db87
Ishaan Javali <ishaanjav@gmail.com>
2020-11-22 14:40:31 -0600

    Create EqualizePricesAgain_1432C.kt

---
+++ EqualizePricesAgain_1432C.kt
@@ -0,0 +1,18 @@
+import java.lang.Math.ceil
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    val test = sc.nextInt()
+    sc.nextLine()
+    for (u in test downTo 1 step 1) {
+        val nums = sc.nextDouble()
+        sc.nextLine()
+        var sum: Double = 0.0
+        for (i in 1..nums.toInt()) {
+            sum += sc.nextDouble()
+        }
+        println(ceil(sum/nums).toInt())
+    }
+
+}
