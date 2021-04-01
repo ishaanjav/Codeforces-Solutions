c5c1d7bc2f5527fd60aebff98f784e65a0822f15
Ishaan Javali <ishaanjav@gmail.com>
2021-04-01 09:58:40 -0500

    Create IsItRated_1505A.java

---
+++ IsItRated_1505A.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.io.IOException;
+ 
+public class IsItRated_1505A {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextLine()){
+            String line = sc.nextLine();
+            System.out.println("NO");
+        }
+    }
+}
